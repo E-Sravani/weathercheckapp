b518ceb5f815cd60c16d9898f378ed0af6d22b9e
E-Sravani <135720121+E-Sravani@users.noreply.github.com>
2023-12-07 21:11:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import Home from './home';
+import Student from './student';
+import StudentList from './studentlist';
+import NestedComment from './commentsection';
+import Employee from './employee';
+import User from './user';
+import Input from './input';
+import WeatherApp from './weatherapp';
+function App() {
+  return (
+  // <div>
+  //   <h1>hello react app</h1> 
+  // </div> 
+  // <Home/>
+  // <Student/>
+  // <StudentList/>
+//<NestedComment/>
+//<Employee/>
+//<user/>
+//<Input type="text" placeholder="enter the name"/>
+<WeatherApp/>
+  
+
+  );
+}
+
+export default App;
---
+++ weatherapp.js
@@ -0,0 +1,54 @@
+// Install Axios for making HTTP requests: npm install axios
+// You need to sign up for a free API key at https://openweathermap.org/appid to use the OpenWeatherMap API.
+
+// App.js
+import React, { useState } from 'react';
+import axios from 'axios';
+import  './styles.css';
+
+const App = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState('');
+
+  const API_KEY = 'e7740192a5d2e88ca26f03fcf30fc6bd';
+
+  const getWeatherData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      setWeatherData(response.data);
+      setError('');
+    } catch (err) {
+      setWeatherData(null);
+      setError('City not found. Please try again.');
+    }
+  };
+
+  return (
+    <div className="app">
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        placeholder="Enter city name"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button onClick={getWeatherData}>Get Weather</button>
+
+      {error && <p className="error">{error}</p>}
+
+      {weatherData && (
+        <div className="weather-info">
+          <h2>{weatherData.name}, {weatherData.sys.country}</h2>
+          <p>{weatherData.weather[0].description}</p>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
